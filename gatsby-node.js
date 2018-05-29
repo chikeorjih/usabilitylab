291e8164ac24e2513f65e551f9d83792644ca933
chike.orjih <chike.orjih@asynchrony.com>
2018-05-29 15:10:54 -0500

    commented out gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
-var fs = require('fs-extra')
-var path = require('path')
+// var fs = require('fs-extra')
+// var path = require('path')
 
-exports.onPostBuild = (pages, callback) => {
-  console.log('Copying .htaccess file')
-  fs.copySync(path.join(__dirname, '/src/config'), path.join(__dirname, '/public/'));
-}
+// exports.onPostBuild = (pages, callback) => {
+//   console.log('Copying .htaccess file')
+//   fs.copySync(path.join(__dirname, '/src/config'), path.join(__dirname, '/public/'));
+// }
