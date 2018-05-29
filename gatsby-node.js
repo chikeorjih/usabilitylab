bab3a0a8231b32ccbb80825092ebfbc746a4828a
chike.orjih <chike.orjih@asynchrony.com>
2018-05-29 13:43:45 -0500

    added htaccess file

---
+++ gatsby-node.js
@@ -0,0 +1,10 @@
+var fs = require('fs-extra')
+var path = require('path')
+
+exports.onPostBuild = (pages, callback) => {
+//   console.log('Copying .htaccess file')
+//   // Copy htaccess.
+//   fs.copySync(path.join(__dirname, '/src/.htaccess'), path.join(__dirname, '/public/'))
+//   // Next callback.
+//   callback()
+}
