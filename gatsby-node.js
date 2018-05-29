66c0843b27e6f9703fd598a71a41bc4d0b9a1339
chike.orjih <chike.orjih@asynchrony.com>
2018-05-29 14:06:56 -0500

    updated build for htaccess

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,9 +2,6 @@ var fs = require('fs-extra')
 var path = require('path')
 
 exports.onPostBuild = (pages, callback) => {
-//   console.log('Copying .htaccess file')
-//   // Copy htaccess.
-//   fs.copySync(path.join(__dirname, '/src/.htaccess'), path.join(__dirname, '/public/'))
-//   // Next callback.
-//   callback()
+  console.log('Copying .htaccess file')
+  fs.copySync(path.join(__dirname, '/src/config'), path.join(__dirname, '/public/'));
 }
